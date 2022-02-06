cc14bb9ce52d2d91b962d8e52e10e393617c4b8d
abelsan <abel@mit.edu>
2022-02-06 16:46:57 -0500

    callbacks distro

---
+++ working_with_data/05_callbacks/01_filter.js
@@ -0,0 +1,11 @@
+var list = [5,8,9,2,7,6,3,1,4];
+
+function callback(list){
+    // to be done
+}
+
+function filter(list, callback){
+    // to be done
+}
+ 
+var filtered = filter(list, callback);
---
+++ working_with_data/05_callbacks/02_filter.js
@@ -0,0 +1,7 @@
+var list = [5,8,9,2,7,6,3,1,4];
+
+function callback(item){
+    // return true for included items
+}
+
+var filtered = list.filter(callback);
